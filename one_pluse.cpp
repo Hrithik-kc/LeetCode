bbbcbecd390ed5804a0bcf5345bddea9aad882da
Hrithik-kc <hrithik.k.cs24@sahyadri.edu.in>
2026-03-15 20:14:17 +0530

    One_pluse

---
+++ one_pluse.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n-1; i>-1; i--) {
+            if (digits[i]<9)
+            {
+                digits[i]=digits[i]+1;
+                return digits;
+            }
+            if(digits[i]==9)   
+                digits[i]=0;      
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+
+            
+    }
+};
