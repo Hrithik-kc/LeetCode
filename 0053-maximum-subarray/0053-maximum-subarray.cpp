52918927129ad7583f8189a23aeb5b2bef050fe1
Hrithik K C <hrithik.k.cs24@sahyadri.edu.in>
2026-07-22 12:03:07 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.89%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+          int n = nums.size();
+           int max_sum = INT_MIN;
+            int current_sum = 0;
+        for (int i = 0; i < n; i++) {
+            current_sum += nums[i];
+            max_sum = max(current_sum,max_sum);
+            if(current_sum<0)
+            {
+                current_sum=0;
+            }
+        }
+        return max_sum;
+    }
+};
