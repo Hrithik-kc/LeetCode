b52c0be1fc47a7605ccc30c958a93e90436932d7
Hrithik K C <hrithik.k.cs24@sahyadri.edu.in>
2026-07-22 13:02:50 +0530

    Time: 11 ms (58.61%), Space: 12.8 MB (83.56%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        h=1;
+        l=1
+        ph=1
+        pl=1
+        m=max(nums)
+        for i in nums:
+            h=i*ph
+            l=i*pl
+            ph=max(h,l,i)
+            pl=min(h,l,i)
+            if(m<ph):
+                m=ph
+        return m
