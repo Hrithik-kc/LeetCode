2de827ba431a389e5acb544b2f254d2c1eb971f3
Hrithik K C <hrithik.k.cs24@sahyadri.edu.in>
2026-08-06 19:35:03 +0530

    Time: 0 ms (100%), Space: 14 MB (85.45%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void Recombination(int ind, vector<int>& candidates, int target,
+                       vector<vector<int>>& ans, vector<int>& ds) {
+
+        if (target == 0) {
+            ans.push_back(ds);
+            return;
+        }
+
+        for (int i = ind; i < candidates.size(); i++) {
+
+            if (i > ind && candidates[i] == candidates[i - 1])
+                continue;
+
+            if (candidates[i] > target)
+                break;
+
+            ds.push_back(candidates[i]);
+
+            Recombination(i + 1, candidates, target - candidates[i], ans, ds);
+
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+
+        sort(candidates.begin(), candidates.end());
+
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        Recombination(0, candidates, target, ans, ds);
+
+        return ans;
+    }
+};
