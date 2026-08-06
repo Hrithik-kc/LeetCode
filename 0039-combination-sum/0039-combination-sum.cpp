935ff9ce3efdad653ca60568d8f12ccd6082ec69
Hrithik K C <hrithik.k.cs24@sahyadri.edu.in>
2026-08-06 19:20:34 +0530

    Time: 0 ms (100%), Space: 14.1 MB (59.82%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
+    void RecCombination(int ind, vector<int>& candidates, int target,
+                        vector<vector<int>>& ans, vector<int>& ds) {
+        if (ind == candidates.size()) {
+            if (target == 0) {
+                ans.push_back(ds);
+            }
+            return;
+        }
+
+        if (candidates[ind] <= target) {
+            ds.push_back(candidates[ind]);
+            RecCombination(ind, candidates, target - candidates[ind], ans, ds);
+            ds.pop_back();
+        }
+
+        RecCombination(ind + 1, candidates, target, ans, ds);
+    }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(candidates.begin(), candidates.end());
+        RecCombination(0, candidates, target, ans, ds);
+        return ans;
     }
 };
