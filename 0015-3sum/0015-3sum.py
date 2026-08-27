6ace2e663b067bdc568df6e07fefe853a272dbea
Hrithik K C <hrithikkc2005@gmail.com>
2026-08-27 18:32:41 +0530

    Time: 753 ms (59.58%), Space: 18.3 MB (81.34%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -9,18 +9,18 @@ class Solution(object):
         for i in range (0,len(nums)-2):
             if i>0  and nums[i]==nums[i-1]:
                 continue
-            j=i+1;
-            k=len(nums)-1;
+            j=i+1
+            k=len(nums)-1
             while j<k:
-                sum=nums[i] + nums[j]+nums[k];
+                sum=nums[i] + nums[j]+nums[k]
                 if sum==0:
-                    ans.append([nums[i],nums[j],nums[k]]);
+                    ans.append([nums[i],nums[j],nums[k]])
                     j+=1
                     k-=1
                     while j<k and nums[j]==nums[j-1]:
-                        j+=1;
+                        j+=1
                     while j<k and nums[k]==nums[k+1]:
-                        k-=1;
+                        k-=1
                 elif sum<0:
                     j+=1
                 
